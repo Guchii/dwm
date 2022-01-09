fcbbe2ff98d1dec32eff22d366b82d1b07131767
Guchii <shvomsrivastava@gmail.com>
2022-01-09 10:29:05 +0530

    change term

--- config.h
+++ config.h
@@ -62,8 +62,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont}; */
-static const char *dmenucmd[] = {"dmenu_apps"};
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = {"dmenu_run"};
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
